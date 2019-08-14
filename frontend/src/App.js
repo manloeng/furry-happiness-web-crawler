eaec36d6177cfb4eeedfdd07f4444acc26567ec5
Mark Cooke <crackerscookie@yahoo.com>
2019-08-14 11:40:38 +0100

    add handleSumbit function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import CreateForm from "./Components/CreateForm";
 
 function App() {
   return (
     <div className="App">
       <p>Home!</p>
+      <CreateForm />
     </div>
   );
 }
