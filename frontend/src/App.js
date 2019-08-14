e8b0ac6218ce50ca5e99dba389edfbd23e6045d6
Mark Cooke <crackerscookie@yahoo.com>
2019-08-14 12:06:50 +0100

    add state set in app of stats

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import CreateForm from "./Components/CreateForm";
 
-function App() {
-  return (
-    <div className="App">
-      <p>Home!</p>
-      <CreateForm />
-    </div>
-  );
+class App extends Component {
+  state = { stats: null };
+
+  render() {
+    return (
+      <div className="App">
+        <p>Home!</p>
+        <CreateForm setStats={this.setStats} />
+      </div>
+    );
+  }
+
+  setStats = stats => {
+    this.setState({ stats });
+  };
 }
 
 export default App;
