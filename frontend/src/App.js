07b97849bf10fd60fbb036005b8dc1d238e3eb50
Sol <you@example.com>
2019-08-14 12:24:01 +0100

    app js still a function

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import ChartDisplay from './ChartDisplay'
 
 function App() {
   return (
     <div className="App">
       <p>Home!</p>
+      
     </div>
   );
 }
