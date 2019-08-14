d1a3da40222c556c730793be292a034803f439c0
Mark Cooke <crackerscookie@yahoo.com>
2019-08-14 11:40:38 +0100

    add handleSumbit function

--- frontend/src/Components/CreateForm.js
+++ frontend/src/Components/CreateForm.js
@@ -4,16 +4,31 @@ class CreateForm extends Component {
   state = {
     url: ""
   };
+
   render() {
+    const { url } = this.state;
     return (
       <div>
-        <form>
-          <input type="text" />
-          <input className={styles.button} type="submit" />
+        <form onSubmit={this.handleSubmit}>
+          <input value={url} type="text" onChange={this.handleTextChange} />
+          <input type="submit" name="submit" />
         </form>
       </div>
     );
   }
+
+  handleTextChange = ({ target: { value } }) => {
+    this.setState({ url: value });
+  };
+
+  handleSubmit = e => {
+    const { url } = this.state;
+    e.preventDefault();
+    api.postURL({ url }).then(data => {
+      console.log(data);
+      this.setState({ url: "" });
+    });
+  };
 }
 
 export default CreateForm;
