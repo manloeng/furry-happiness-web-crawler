5ec48e8b288dba1a7696883a34e1668411730d68
Huw Jones <huwjones025@gmail.com>
2019-08-14 11:56:36 +0100

    delete incorrect files

--- controllers/apiController.js
+++
@@ -1,7 +0,0 @@
-const doStuff = require('../models/apiModel');
-
-const getLinks = (req, res, next) => {
-  doStuff(req.body)
-}
-
-module.exports = getLinks;
--- listen.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-
-const PORT = process.env.PORT || 9090;
-
-app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-})
